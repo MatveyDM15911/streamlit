4b151aa24de3d50c34b5e92391c0e8fe37f9ad60
MatveyDM15911 <125370741+MatveyDM15911@users.noreply.github.com>
2025-06-15 18:16:27 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,4 @@ import streamlit as st
 from streamlit_js_eval import streamlit_js_eval
 
 result = streamlit_js_eval(js_expressions="window.Telegram?.WebApp?.initDataUnsafe?.user?.id", key="get_user_id")
-user_id = result["data"]
-
-if user_id:
-    st.success(f"Ваш Telegram user_id: {user_id}")
-else:
-    st.info("Откройте приложение через Telegram Mini App")
+st.write(result)
