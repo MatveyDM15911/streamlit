61ecb2ee376dc911cacfce77d8449a0cbc977bd7
MatveyDM15911 <125370741+MatveyDM15911@users.noreply.github.com>
2025-06-15 18:29:17 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from streamlit_js_eval import streamlit_js_eval
 
-result = streamlit_js_eval(js_expressions="window.Telegram?.WebApp?.initDataUnsafe?.user?.id", key="get_user_id")
-st.write(result)
+result = streamlit_js_eval(js_expressions="window.Telegram?.WebApp?.initDataUnsafe", key="get_init_data")
+st.write("initDataUnsafe:", result)
