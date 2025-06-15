2e57e2d409824589be95d028862aa4ae0fdabe9a
MatveyDM15911 <125370741+MatveyDM15911@users.noreply.github.com>
2025-06-15 18:44:18 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
 params = st.query_params()
-user_id = params.get("user_id", [None])[0]
 
-if user_id:
-    st.success(f"Ваш Telegram user_id: {user_id}")
-else:
-    st.warning("user_id не найден в параметрах URL")
+st.write(params)
